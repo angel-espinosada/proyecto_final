7051b586bf382b9c169845f376187582990b876f
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-11-28 22:02:46 -0500

    Agrgando escena y elementos al escenario como pruebas

--- Tubos_de_vacio/mainwindow.cpp
+++ Tubos_de_vacio/mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsRectItem>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+   QGraphicsScene *escenario = new QGraphicsScene(this);
+    escenario->setSceneRect(0,0,570,410);
+    ui->graphicsView->setScene(escenario);
+    QGraphicsRectItem *cuadrado = new QGraphicsRectItem(150, 100, 10, 100);
+    QGraphicsRectItem *cuadrado1 = new QGraphicsRectItem(-150, 100, 10, 100);
+    cuadrado->setBrush(Qt::red);
+    cuadrado1->setBrush(Qt::green);
+    escenario->addItem(cuadrado);
+    escenario->addItem(cuadrado1);
 }
 
 MainWindow::~MainWindow()
--- Tubos_de_vacio/mainwindow.h
+++ Tubos_de_vacio/mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QGraphicsScene>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
